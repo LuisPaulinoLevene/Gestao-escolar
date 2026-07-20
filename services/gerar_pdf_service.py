fcf72c805204ea57d541f70cfb4e15291051ac21
LuisPaulinoLevene <136761438+LuisPaulinoLevene@users.noreply.github.com>
2026-07-20 23:53:13 +0200

    Update gerar_pdf_service.py

--- services/gerar_pdf_service.py
+++ services/gerar_pdf_service.py
@@ -1,46 +1,16 @@
-from playwright.sync_api import sync_playwright
-
-
-CHROME_PATH = (
-    "/opt/render/.cache/ms-playwright/"
-    "chromium-1228/chrome-linux64/chrome"
-)
+import os
+import glob
 
 
 def gerar_pdf(html: str) -> bytes:
 
-    with sync_playwright() as p:
+    print("=== LISTANDO PLAYWRIGHT ===")
 
-        browser = p.chromium.launch(
-            headless=True,
-            executable_path=CHROME_PATH,
-            args=[
-                "--no-sandbox",
-                "--disable-dev-shm-usage",
-                "--disable-gpu"
-            ]
-        )
+    for caminho in glob.glob(
+        "/opt/render/.cache/ms-playwright/**/*",
+        recursive=True
+    ):
+        if "chrome" in caminho:
+            print(caminho)
 
-        pagina = browser.new_page()
-
-        pagina.set_content(
-            html,
-            wait_until="networkidle"
-        )
-
-        pagina.wait_for_timeout(1000)
-
-        pdf = pagina.pdf(
-            format="A4",
-            print_background=True,
-            margin={
-                "top": "10mm",
-                "bottom": "10mm",
-                "left": "10mm",
-                "right": "10mm"
-            }
-        )
-
-        browser.close()
-
-        return pdf
+    raise Exception("Teste de caminho concluído")
